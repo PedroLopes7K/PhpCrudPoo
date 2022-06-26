7fd30b3cce6c3ba30048933fa59063a33c659047
PedroLopes7K <pedrohenri3601@gmail.com>
2022-06-25 21:43:37 -0300

    Final Crud

--- index.php
+++ index.php
@@ -5,12 +5,21 @@ use App\Model\Product;
 require_once 'vendor/autoload.php';
 
 
-$product = new \App\Model\Product();
-$product->setName('SAMSUNG J4');
-$product->setDescription('Color BLACK, 24GB');
+$newProduct = new \App\Model\Product();
+$newProduct->setId(6);
+$newProduct->setName('TENIS NIKE');
+$newProduct->setDescription('Color Red, 42');
 
 
 $productDao = new \App\Model\ProductDao();
-$productDao->create($product);
-
+// $productDao->create($newProduct);
+$productDao->update($newProduct);
 $allProducts = $productDao->read();
+// var_dump($allProducts);
+foreach ($allProducts as $product) {
+  echo "<h3> Product: " . $product['name'] . "</h2> ";
+  echo "<span> Description: " . $product['description'] . "</span> <br>";
+  echo "<hr>";
+}
+
+$productDao->delete($newProduct->getId());
